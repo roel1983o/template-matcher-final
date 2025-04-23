58d5e9af83e9e1e53e4339a2a39d334acb3f97a7
roel1983o <roel.ophelders@delimburger.nl>
2025-04-24 00:47:42 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,20 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // project root
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  root: '.',
   build: {
     rollupOptions: {
-      input: resolve(__dirname, 'index.html') // expliciet entry point
+      input: resolve(__dirname, 'index.html')
     },
     outDir: 'dist'
   }
-})
+});
